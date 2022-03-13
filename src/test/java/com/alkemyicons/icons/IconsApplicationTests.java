8dded7d633ea8093decde8420ce0d027b8bf6f64
giovistica <giovi.rhcp@gmail.com>
2022-03-13 16:50:12 -0300

    Initial commit

---
+++ src/test/java/com/alkemyicons/icons/IconsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemyicons.icons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
